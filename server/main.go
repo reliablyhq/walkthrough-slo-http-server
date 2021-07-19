2380358e26338c62bbb801e5b98d538efc8d3224
mick-roper <mick@theropers.co.uk>
2021-07-19 14:33:27 +0100

    wired everything up
    Signed-off-by: mick-roper <mick@theropers.co.uk>

--- server/main.go
+++ server/main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"os"
 	"strconv"
 	"time"
 
@@ -15,21 +17,28 @@ const (
 )
 
 func main() {
+	var portString string
+	if port, err := strconv.Atoi(os.Getenv("PORT")); err == nil {
+		portString = fmt.Sprintf(":%d", port)
+	} else {
+		log.Fatal(err)
+	}
+
 	router := httprouter.New()
 
 	router.GET("/", index)
 
-	log.Fatal(http.ListenAndServe(":8080", router))
+	log.Fatal(http.ListenAndServe(portString, router))
 }
 
 func index(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 	var delay time.Duration
-	if l, err := time.ParseDuration(r.URL.Query().Get(paramLatency)); err != nil {
+	if l, err := time.ParseDuration(r.URL.Query().Get(paramLatency)); err == nil {
 		delay = l
 	}
 
 	statusCode := 200
-	if s, err := strconv.Atoi(r.URL.Query().Get(paramStatusCode)); err != nil {
+	if s, err := strconv.Atoi(r.URL.Query().Get(paramStatusCode)); err == nil {
 		statusCode = s
 	}
 
