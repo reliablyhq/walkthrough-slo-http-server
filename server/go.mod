a68ef34a90ce413801164a1ad979c1caf31f97c7
mick-roper <mick@theropers.co.uk>
2021-07-19 14:26:36 +0100

    added all the required shonk
    Signed-off-by: mick-roper <mick@theropers.co.uk>

--- server/go.mod
+++ server/go.mod
@@ -1,3 +1,5 @@
 module reliablyhq/walkthrough-slo-http-app/server
 
 go 1.16
+
+require github.com/julienschmidt/httprouter v1.3.0
--- server/main.go
+++ server/main.go
@@ -1,5 +1,39 @@
 package main
 
-func main() {
+import (
+	"log"
+	"net/http"
+	"strconv"
+	"time"
 
+	"github.com/julienschmidt/httprouter"
+)
+
+const (
+	paramLatency    = "latency"
+	paramStatusCode = "statuscode"
+)
+
+func main() {
+	router := httprouter.New()
+
+	router.GET("/", index)
+
+	log.Fatal(http.ListenAndServe(":8080", router))
+}
+
+func index(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	var delay time.Duration
+	if l, err := time.ParseDuration(r.URL.Query().Get(paramLatency)); err != nil {
+		delay = l
+	}
+
+	statusCode := 200
+	if s, err := strconv.Atoi(r.URL.Query().Get(paramStatusCode)); err != nil {
+		statusCode = s
+	}
+
+	time.Sleep(delay)
+
+	w.WriteHeader(statusCode)
 }
