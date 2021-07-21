0be337c2ffcdf6289453dc03e0f190879426bf53
Mick Roper <mick@theropers.co.uk>
2021-07-21 15:01:44 +0100

    Ramped up the logging

--- server/main.go
+++ server/main.go
@@ -17,6 +17,8 @@ const (
 )
 
 func main() {
+	log.Println("server starting...")
+
 	var portString string
 	if port, err := strconv.Atoi(os.Getenv("PORT")); err == nil {
 		portString = fmt.Sprintf(":%d", port)
@@ -25,22 +27,28 @@ func main() {
 	}
 
 	router := httprouter.New()
-
 	router.GET("/", index)
 
+	log.Println("server listening at", portString)
 	log.Fatal(http.ListenAndServe(portString, router))
 }
 
 func index(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	log.Println(r.Method, r.URL.String())
+
 	var delay time.Duration
 	if l, err := time.ParseDuration(r.URL.Query().Get(paramLatency)); err == nil {
 		delay = l
 	}
 
+	log.Println("delaying by", delay.String())
+
 	statusCode := 200
 	if s, err := strconv.Atoi(r.URL.Query().Get(paramStatusCode)); err == nil {
 		statusCode = s
 	}
+
+	log.Println("returning status code", statusCode)
 
 	time.Sleep(delay)
 
